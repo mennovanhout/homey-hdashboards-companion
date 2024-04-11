7fd1b1830b9210860fbf46c9ed2e048a0e494595
Menno van Hout <info@mennovanhout.nl>
2024-04-11 21:56:13 +0200

    add cards for refresh and open dashboards

--- app.ts
+++ app.ts
@@ -1,6 +1,10 @@
 import Homey from 'homey';
 import axios from 'axios';
 
+interface ColorMemory {
+  [key: string]: string | null;
+}
+
 class HDashboardsCompanionApp extends Homey.App {
 
   sendNotificationCard: Homey.FlowCardAction|undefined;
@@ -9,6 +13,9 @@ class HDashboardsCompanionApp extends Homey.App {
   sendImagePersistentNotificationCard: Homey.FlowCardAction|undefined;
   setCardBackgroundColor: Homey.FlowCardAction|undefined;
   resetCardBackgroundColor: Homey.FlowCardAction|undefined;
+  openDashboardForUser: Homey.FlowCardAction|undefined;
+  refreshDashboardForUser: Homey.FlowCardAction|undefined;
+  colorMemory: ColorMemory = {};
 
   async onInit() {
     this.sendNotificationCard = this.homey.flow.getActionCard('send-notification-to-hdashboards');
@@ -29,7 +36,97 @@ class HDashboardsCompanionApp extends Homey.App {
     this.resetCardBackgroundColor = this.homey.flow.getActionCard('reset-card-background-color');
     this.resetCardBackgroundColor.registerRunListener(this.sendSetCardBackgroundColorToHDashboards.bind(this));
 
+    this.openDashboardForUser = this.homey.flow.getActionCard('open-dashboard-for-user');
+    this.openDashboardForUser.registerArgumentAutocompleteListener('user', this.userAutocompleteListener.bind(this));
+    this.openDashboardForUser.registerArgumentAutocompleteListener('dashboard', this.dashboardAutocompleteListener.bind(this));
+    this.openDashboardForUser.registerRunListener(this.sendOpenDashboardCommand.bind(this));
+
+    this.openDashboardForUser = this.homey.flow.getActionCard('refresh-dashboard-for-user');
+    this.openDashboardForUser.registerArgumentAutocompleteListener('user', this.userAutocompleteListener.bind(this));
+    this.openDashboardForUser.registerRunListener(this.sendRefreshDashboardCommand.bind(this));
+
     this.log('HDashboards companion app has been initialized');
+  }
+
+  private sendRefreshDashboardCommand(query: string, args: any) {
+    const key = this.homey.settings.get('key');
+
+    if (key === undefined || key === null || key === '') {
+      throw new Error('Please enter a key in app settings');
+    }
+
+    this.homey.api.realtime('hdashboards:refresh-dashboard', query);
+  }
+
+  private sendOpenDashboardCommand(query: string, args: any) {
+    const key = this.homey.settings.get('key');
+
+    if (key === undefined || key === null || key === '') {
+      throw new Error('Please enter a key in app settings');
+    }
+
+    this.homey.api.realtime('hdashboards:open-dashboard', query);
+  }
+
+  private async userAutocompleteListener(query: string, args: any) {
+    const key = this.homey.settings.get('key');
+
+    if (key === undefined || key === null || key === '') {
+      throw new Error('Please enter a key in app settings');
+    }
+
+    try {
+      const data = await axios.get('https://hdashboards.app/companion-api/users', {
+        headers: {
+          'x-api-key': key,
+        },
+      });
+
+      const results = data.data.map((result: any) => {
+        return {
+          name: `${result.first_name} ${result.last_name}`,
+          description: result.email,
+          id: result.id,
+        };
+      });
+
+      return results.filter((result: any) => {
+        return result.name.toLowerCase().includes(query.toLowerCase());
+      });
+    } catch (error) {
+      // @ts-ignore
+      throw new Error(error.message);
+    }
+  }
+
+  private async dashboardAutocompleteListener(query: string, args: any) {
+    const key = this.homey.settings.get('key');
+
+    if (key === undefined || key === null || key === '') {
+      throw new Error('Please enter a key in app settings');
+    }
+
+    try {
+      const data = await axios.get('https://hdashboards.app/companion-api/dashboards', {
+        headers: {
+          'x-api-key': key,
+        },
+      });
+
+      const results = data.data.map((result: any) => {
+        return {
+          name: result.name,
+          id: result.uuid,
+        };
+      });
+
+      return results.filter((result: any) => {
+        return result.name.toLowerCase().includes(query.toLowerCase());
+      });
+    } catch (error) {
+      // @ts-ignore
+      throw new Error(error.message);
+    }
   }
 
   private sendNotificationToHDashboards(args: any, stats: any) {
@@ -56,6 +153,14 @@ class HDashboardsCompanionApp extends Homey.App {
     if (!args.backgroundColor) {
       args.backgroundColor = null;
     }
+
+    // Check if the color is the same as the previous one
+    if (this.colorMemory[args.identifier] === args.backgroundColor) {
+      return true;
+    }
+
+    // Save color
+    this.colorMemory[args.identifier] = args.backgroundColor;
 
     // Send over socket
     this.homey.api.realtime('hdashboards:card-color', args);
